901b89d40d783525da6e4bd064fe0359589ebac5
mat_sama <mateosejourne@gmail.com>
2025-08-24 22:06:06 +0200

    feat(search user):add

--- routes/user.js
+++ routes/user.js
@@ -15,6 +15,17 @@ router.get('/:userId/publicKey', async (req, res) => {
     }
 });
 
+router.get('/:username/:code/id', async (req, res) => {
+    const { username, code } = req.params;
+    try {
+        const user = await UserManager.getUserByFullUsername(username, Number(code));
+        res.json({ userId: user ? user.uniqueId : null });
+    } catch (error) {
+        Log.Error("Error fetching user ID:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 router.get('/:userId/username', async (req, res) => {
     const { userId } = req.params;
     try {
