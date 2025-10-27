a2e76df2c07db2992ca2cf1f5bbfd7ad81593074
mat_sama <mateosejourne@gmail.com>
2025-10-27 01:27:40 +0100

    fix(response server of /auth):update

--- routes/auth.js
+++ routes/auth.js
@@ -11,35 +11,42 @@ router.post('/register', async (req, res) => {
     const { email, username, tag, password, passwordConfirmation, publicKey } = req.body;
 
     if (!MailValidator.validateEmail(email)) {
-        return res.status(400).json({ message: "Invalid email format" });
+        return res.status(400).json({ success: false, message: "Invalid email format" });
     }
 
     if (await UserManager.getUserByEmail(email)) {
-        return res.status(400).json({ message: "Email already in use" });
+        return res.status(400).json({ success: false, message: "Email already in use" });
     }
 
     if (await UserManager.getUserByFullUsername(username, tag)) {
-        return res.status(400).json({ message: "Username and tag already in use" });
+        return res.status(400).json({ success: false, message: "Username and tag already in use" });
     }
 
     if (!PasswordValidator.isValidPassword(password)) {
-        return res.status(400).json({ message: "Invalid password format" });
+        return res.status(400).json({ success: false, message: "Invalid password format" });
     }
 
     if (!PasswordValidator.isSamePassword(password, passwordConfirmation)) {
-        return res.status(400).json({ message: "Passwords do not match" });
+        return res.status(400).json({ success: false, message: "Passwords do not match" });
     }
 
     if (!publicKey || typeof publicKey !== 'string') {
-        return res.status(400).json({ message: "Invalid public key" });
+        return res.status(400).json({ success: false, message: "Invalid public key" });
     }
 
     try {
         const user = await UserManager.createUser({ email, username, tag, password, publicKey });
         const auth = await AuthManager.createAuth(user.uniqueId);
-        return res.status(201).json({ message: "User registered successfully", token: auth.token, user: user });
+        return res.status(201).json({ 
+            success: true, 
+            message: "User registered successfully", 
+            datas: {
+                token: auth.token, 
+                user: user 
+            }
+        });
     } catch (error) {
-        return res.status(500).json({ message: error.message || "Internal server error" });
+        return res.status(500).json({ success: false, message: error.message || "Internal server error" });
     }
 });
 
@@ -47,19 +54,19 @@ router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     if (!MailValidator.validateEmail(email)) {
-        return res.status(400).json({ message: "Invalid email format" });
+        return res.status(400).json({ success: false, message: "Invalid email format" });
     }
 
     try {
         // check if user exists
         const user = await UserManager.getUserByEmail(email);
         if (!user) {
-            return res.status(400).json({ message: "User not found" });
+            return res.status(400).json({ success: false, message: "User not found" });
         }
 
         // compare hashed password and password
         if (!await bcrypt.compare(password, user.password)) {
-            return res.status(400).json({ message: "Invalid password" });
+            return res.status(400).json({ success: false, message: "Invalid password" });
         }
 
         // check if user has auth
@@ -71,9 +78,15 @@ router.post('/login', async (req, res) => {
 
         // create new auth
         const auth = await AuthManager.createAuth(user.uniqueId);
-        return res.status(200).json({ message: "Login successful", token: auth.token, user: user });
+        return res.status(200).json({ 
+            success: true,
+            message: "Login successful", 
+            datas: {
+                token: auth.token, user: user 
+            }
+        });
     } catch (error) {
-        return res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ success: false, message: "Internal server error" });
     }
 })
 
